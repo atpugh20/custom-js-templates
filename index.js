2d9549ddc5cb0f13903269f7e5dc2e148a66dc8c
Alex <133706872+atpugh20@users.noreply.github.com>
2026-05-11 10:37:46 -0700

    chore: update comments for better documentation

--- index.js
+++ index.js
@@ -6,33 +6,37 @@ document.addEventListener("DOMContentLoaded", () => {
     const srcElements = document.querySelectorAll("[data-src]");
     const hrefElements = document.querySelectorAll("[data-href]");
 
-    // Content Templates
+    // Replace the innerHTML of every element with "data-content" attribute
     if (window.contentVars) {
         for (let e of contentElements) {
             const key = e.dataset.content;
 
             if (window.contentVars[key]) {
                 e.innerHTML = window.contentVars[key];
+            } else {
+                console.log(`${key} not found in window.contentVars.`);
             }
         }
     } else {
         console.log("window.contentVars array not found.");
     }
 
-    // Image Source Templates
+    // Replace the src of every element with a "data-src" attribute
     if (window.srcVars) {
         for (let e of srcElements) {
             const key = e.dataset.src;
 
             if (window.srcVars[key]) {
                 e.src = window.srcVars[key];
+            } else {
+                console.log(`${key} not found in window.srcVars.`);
             }
         }
     } else {
         console.log("window.srcVars array not found.");
     }
 
-    // Website Link Templates
+    // Replace the navigation link of every element with a "data-href" attribute
     if (window.hrefVars) {
         for (let e of hrefElements) {
             const key = e.dataset.href;
@@ -40,6 +44,8 @@ document.addEventListener("DOMContentLoaded", () => {
             if (window.hrefVars[key]) {
                 e.href = window.hrefVars[key];
                 e.target = "_blank";
+            } else {
+                console.log(`${key} not found in window.hrefVars.`);
             }
         }
     } else {
